f84d3e8121ca358b339d5b15687933cecf03b649
JuliePolepishinaEkaterinburg11A <juliejanika1208@gmail.com>
2025-03-14 00:29:34 +0500

    Update TriangleChecker.py

--- TriangleChecker/TriangleChecker.py
+++ TriangleChecker/TriangleChecker.py
@@ -1,1 +1,7 @@
-
+a=float(input('Введите первую сторону'))
+b=float(input('Введите вторую сторону'))
+c=float(input('Введите третью сторону'))
+if (a+b>c) and (b+c>a) and (c+a>b):
+  print('Существует')
+else:
+  print('Не существует')
